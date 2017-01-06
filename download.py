3ea2785be13c268b336a8f909a444cb25a898d0c
fredrikbryntesson <fredrik.bryntesson@vidhance.com>
2017-01-06 19:59:05 +0100

    Updated download wrapper

--- download.py
+++ download.py
@@ -3,11 +3,23 @@ import sys
 
 input = sys.argv[1]
 output = subprocess.check_output(["./youtube-dl",'-F', input])
-
 lines = output.split('\n')
 stream = lines[len(lines)-2].split(' ')[0]
 print(stream)
-cmd = ['./youtube-dl', '-f', stream, input]
-subprocess.call(cmd)
+print(input)
+downloadCommand = ['./youtube-dl', '-f', stream, input]
+subprocess.check_output(downloadCommand)
 
+if(len(sys.argv) < 3):
+        print('easy')
+else:
+        print('hard')
+        destFolder = '/home/fredrik/' + sys.argv[2] + '/' + sys.argv[3] + '-'
+        nameCommand = ['./youtube-dl', '--get-filename',  input]
+        getFilename = subprocess.check_output(nameCommand)
+        name = getFilename.strip('\n')
+        print(name)
+        print(destFolder)
+        moveCommand = ['mv', name, destFolder+name]
+        subprocess.call(moveCommand)
 
